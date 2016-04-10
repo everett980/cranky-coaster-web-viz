fdd3f72827126735210bd794e7b1ede199c63a60
everett980 <everett980@gmail.com>
2016-04-09 23:18:27 -0400

    visualizes single days and last 5 days with code that utilizes nifty javascript subtleties

--- src/containers/DataViz/DataViz.js
+++ src/containers/DataViz/DataViz.js
@@ -1,22 +1,84 @@
 import React, {Component} from 'react';
 import Helmet from 'react-helmet';
+import { BarChart } from 'react-d3';
+import moment from 'moment';
+
+let barData = [
+	{
+		'name': 'Series A',
+		'values': [
+			{'x': 1, 'y': 4},
+		],
+	}
+]
+
+let barData2 = [
+	{
+		'name': 'Series A',
+		'values': [
+			{'x': 1, 'y': 4},
+		],
+	}
+]
+
+let sipsArr;
+let todaySips;
+let todayChartData;
+let lastFiveDaysNoToday;
+let lastFiveChartData;
 
 export default class DataViz extends Component {
-
-  componentWillMount() {
-	  console.log('about to fetch');
-	  try { 
-	  fetch(`https://lit-savannah-65925.herokuapp.com/api/cupReadings`);
-	  } catch(e) {
-		  console.error(e);
-	  }
-  }
+  
+  componentDidMount() {
+	  fetch(`https://crankycoaster.firebaseio.com/.json`).then(data => data.json()).then(sipObj => {
+		sipsArr = sipObj[Object.keys(sipObj)[0]];
+	  	todaySips = sipsArr.filter((sip) => {
+			return moment(sip.time).date() === moment().date();
+		});
+		todayChartData = todaySips.reduce((prev, sip) => {
+			const xVal = moment(sip.time).hour();
+			prev[xVal] = {
+				x: xVal,
+				y: sip.changeInForce + ( ( prev[xVal] && prev[xVal].y) || 0)
+			}
+			return prev;
+		}, []);
+		barData = [
+  			{'name': 'Series B',
+			 'values': todayChartData
+			}
+		];
+		
+		lastFiveDaysNoToday = sipsArr.filter((sip) => {
+			const dif = moment().date() - moment(sip.time).date();
+			return dif && dif < 6;
+		});
+		const minDay = lastFiveDaysNoToday.reduce((prev, sip) => {
+			return prev < moment(sip.time).date() ? prev : moment(sip.time).date();
+		}, moment(lastFiveDaysNoToday[0].time).date());
+		lastFiveChartData = lastFiveDaysNoToday.reduce((prev, sip) => {
+			const xVal = moment(sip.time).date() - minDay;
+			prev[xVal] = {
+				x: xVal,
+				y: sip.changeInForce + ( ( prev[xVal] && prev[xVal].y ) || 0 )
+			}
+			return prev;
+		}, []);
+		barData2 = [
+		{'name': 'Series B',
+			'values': lastFiveChartData
+			}
+		];
+	});
+  }	
   render() {
     return (
       <div className="container">
         <h1>Data Visualization</h1>
         <Helmet title="Data Visualization"/>
 		View Datas Dude
+		<BarChart data={barData} title={`Today's Drinking`} xAxisLabel={`Hour (Military Time)`} yAxisLabel={`Force, for now`}/>
+		<BarChart data={barData2} title={`Last Five Days' Drinking`} xAxisLabel={`Hour (Military Time)`} yAxisLabel={`Force, for now`}/>
       </div>
     );
   }
